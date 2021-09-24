9f1da854746c2f65a506cdb609207a79db3aed13
Wilson Chu <wilsnchu@berkeley.edu>
2021-09-24 00:37:22 -0700

    Wilson's implemented scenes

--- Assets/Scripts/UI/MainSceneTitle.cs
+++ Assets/Scripts/UI/MainSceneTitle.cs
@@ -2,22 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainSceneTitle : MonoBehaviour
 {
     [SerializeField] private TextGenerator textGenerator;
-    [SerializeField] private float charDisplaySeed;
-    private Text messageText;
-    // Start is called before the first frame update
+    [SerializeField] private float charDisplaySpeed;
+    private Text titleText;
+    private Text startText;
+    private bool created = false;
     private void Awake()
     {
-        messageText = transform.Find("TitleText").GetComponent<Text>();
+        titleText = transform.Find("TitleText").GetComponent<Text>();
+        startText = transform.Find("StartButton").Find("StartText").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Start()
     {
-        TextGenerator textGeneratorOne = textGenerator;
-        textGeneratorOne.AddWriter(messageText, "Lights     Out", charDisplaySeed);
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "MainScene")
+        {
+            textGenerator.AddWriter(titleText, "Lights     Out", charDisplaySpeed);
+            textGenerator.AddWriter(startText, "Start", charDisplaySpeed);
+        } else if (scene.name == "VictoryScene")
+        {
+            textGenerator.AddWriter(titleText, "Congratulations!!!", charDisplaySpeed);
+            textGenerator.AddWriter(startText, "Start   again", charDisplaySpeed);
+        } else if (scene.name == "DeadScene")
+        {
+            textGenerator.AddWriter(titleText, "You     lost...", charDisplaySpeed);
+            textGenerator.AddWriter(startText, "Try again", charDisplaySpeed);
+        }
     }
 }
